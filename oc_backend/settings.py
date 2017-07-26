9d9cf4b44109fef4775df2d0ca5f5ca64a2e06d6
Donny Lawrence <trea@treamous.com>
2017-07-26 09:45:28 -0500

    Set up a basic auth system.

--- oc_backend/settings.py
+++ oc_backend/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+LOGIN_REDIRECT_URL = "/app/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'oc_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates/oc_server'), os.path.join(BASE_DIR, 'templates/oc_api')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- oc_backend/urls.py
+++ oc_backend/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from oc_server import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login/', views.login_page),
-    url(r'^register/', views.register_page)
+    url(r'^$', auth_views.LoginView.as_view())
+    # url(r'^login/', views.login_page),
+    # url(r'^register/', views.register_page)
 ]
