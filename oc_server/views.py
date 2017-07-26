486b7c08b8ac98ae842a2b3643f80bdb409669a5
Donny Lawrence <trea@treamous.com>
2017-07-26 09:51:43 -0500

    Rearrange some template files, add the login view.

--- oc_server/views.py
+++ oc_server/views.py
@@ -5,35 +5,35 @@ from django.contrib.auth.models import User
 
 
 # Create your views here.
-def login_page(request):
-    if request.POST:
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            html = "<p>You've been logged in</p>"
-            return HttpResponse(html)
-        else:
-            html = """
-            <p>Invalid login</p>
-            """
-            return HttpResponse(html)
-    else:
-        return render(request, "oc_server/login.html")
-
-
-# Create your views here.
-def register_page(request):
-    if request.POST:
-        email = request.POST["email"]
-        username = request.POST["username"]
-        password = request.POST["password"]
-
-        user = User.objects.create_user(username, email, password)
-
-        html = "<p>Successfully created user</p>"
-        return HttpResponse(html)
-
-    else:
-        return render(request, "oc_server/register.html")
+# def login_page(request):
+#     if request.POST:
+#         username = request.POST["username"]
+#         password = request.POST["password"]
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             html = "<p>You've been logged in</p>"
+#             return HttpResponse(html)
+#         else:
+#             html = """
+#             <p>Invalid login</p>
+#             """
+#             return HttpResponse(html)
+#     else:
+#         return render(request, "oc_server/login.html")
+#
+#
+# # Create your views here.
+# def register_page(request):
+#     if request.POST:
+#         email = request.POST["email"]
+#         username = request.POST["username"]
+#         password = request.POST["password"]
+#
+#         user = User.objects.create_user(username, email, password)
+#
+#         html = "<p>Successfully created user</p>"
+#         return HttpResponse(html)
+#
+#     else:
+#         return render(request, "oc_server/register.html")
