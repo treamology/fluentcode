1f04adb46410eb0aa6f05124e1b177e8036a18f3
Donny Lawrence <trea@treamous.com>
2017-07-26 10:25:56 -0500

    Add test view to oc_api

--- oc_backend/urls.py
+++ oc_backend/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from oc_server import views
+from oc_api import views as api_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', auth_views.LoginView.as_view())
+    url(r'^$', auth_views.LoginView.as_view()),
+
+    url('^api/hello/', api_views.TestView.as_view())
     # url(r'^login/', views.login_page),
     # url(r'^register/', views.register_page)
 ]
