da2136e9b725728d41c7f88ef3ed44ebb122fece
Donny Lawrence <trea@treamous.com>
2017-07-26 10:58:43 -0500

    Move URLs into the apps themselves.

--- oc_backend/urls.py
+++ oc_backend/urls.py
@@ -13,16 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from oc_api import views as api_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', auth_views.LoginView.as_view()),
-
-    url('^api/hello/', api_views.TestView.as_view())
+    url(r'^', include("oc_server.urls")),
+    url(r'^api/', include("oc_api.urls"))
     # url(r'^login/', views.login_page),
     # url(r'^register/', views.register_page)
 ]
