20283d8e648239a26941940517dd59e3d614a3fe
Donny Lawrence <trea@treamous.com>
2017-07-27 11:52:35 -0500

    Start serving react app through django server.

--- oc_server/urls.py
+++ oc_server/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from oc_server import views as server_views
 from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles import views as static_views
+from oc_backend import settings
 
 urlpatterns = [
     url(r'^$', auth_views.LoginView.as_view()),
+    url(r'^app/', server_views.app_view),
 ]
