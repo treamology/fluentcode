344523020ac65917f5b18eada05a35b881ffd7ef
Donny Lawrence <trea@treamous.com>
2017-08-04 13:26:27 -0500

    Add basic celery task handling test.

--- oc_api/views.py
+++ oc_api/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .tasks import add
+
+
 class TestView(APIView):
     def get(self, request, format=None):
+        add.delay(2, 2)
         return Response("{ 'hello': 'hey there' }")
 
 # Create your views here.
