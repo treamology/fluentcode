20e7974cd1a74fad3fd8def21b5cbbcd4bfb73d4
Donny Lawrence <trea@treamous.com>
2017-10-06 16:30:18 -0500

    Create basic CSS grid.

---
+++ src/OCSite.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './App.css';
+
+class OCSite extends React.Component {
+  render() {
+    return (
+      <div className="gridContainer">
+        <div className="header">
+        </div>
+        <div className="docSidebar">
+        </div>
+        <div className="dragSidebar">
+        </div>
+        <div className="output">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default OCSite;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import OCSite from './OCSite';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <OCSite />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
