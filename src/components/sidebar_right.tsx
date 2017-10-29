3311decc1d34723a517a5fabfec109627009108e
Donny Lawrence <trea@treamous.com>
2017-10-29 18:12:24 -0500

    Specify default font family.

--- src/components/sidebar_right.tsx
+++ src/components/sidebar_right.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import '../App.scss';
 
 import { PanelHeader } from './small_ui';
+import Draggable from './draggable';
 
 class SidebarRight extends React.Component {
     render() {
         return (
             <div className="sidebarRight">
                 <PanelHeader headerText="Code Blocks" />
+                <Draggable codeTitleText="if" nonCodeTitleText="statement" descriptionText="Something about an if statement."/>
             </div>
         );
     }
