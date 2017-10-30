6405f8780047dae1964eb8c0de8b849c97b443a0
Donny Lawrence <trea@treamous.com>
2017-10-29 19:59:26 -0500

    Simplify some structure.

--- src/OCSite.tsx
+++ src/OCSite.tsx
@@ -2,19 +2,33 @@ import * as React from 'react';
 import './App.scss';
 
 import Header from './components/header';
-import SidebarLeft from './components/sidebar_left';
-import SidebarRight from './components/sidebar_right';
 import OutputPanel from './components/output_panel';
 import CodeEditor from './components/code_editor';
+import { PanelHeader } from './components/small_ui';
 
-class OCSite extends React.Component {
+//import { CodeBlock } from './state/types';
+
+interface OCSiteProps {
+  // currentDraggables: Array<CodeBlock>;
+}
+
+class OCSite extends React.Component<OCSiteProps> {
   render() {
     return (
       <div className="gridContainer">
         <Header />
         <CodeEditor />
-        <SidebarLeft />
-        <SidebarRight />
+        <div className="sidebarLeft">
+            <div className="courseOutlineSection">
+                <PanelHeader headerText="Course Outline" />
+            </div>
+            <div className="lessonContentSection">
+                <PanelHeader headerText="Current Lesson" />
+            </div>
+        </div>
+        <div className="sidebarRight">
+          <PanelHeader headerText="Code Blocks" />
+        </div>
         <OutputPanel />
       </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Store } from 'redux';
 import OCSite from './OCSite';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+import { createStore } from 'redux';
+import ocSite from './state/reducer';
+import { ApplicationState } from './state/types'
+
+let store: Store<ApplicationState> = createStore(ocSite)
+
 ReactDOM.render(
-  <OCSite />,
+  <Provider store={store}>
+    <OCSite />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
