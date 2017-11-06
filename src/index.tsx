d9f5f7f41a6b3b28426e613788fcd6e2b9484d8c
Donny Lawrence <trea@treamous.com>
2017-11-06 11:19:40 -0600

    Add some sample data, change the padding of the draggables.

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,20 @@ let initialState: ApplicationState = { visibleCodeBlocks:
   [{codeTitleText: 'if', 
   nonCodeTitleText: 'statement', 
   descriptionText: 'Something about an if statement.',
-  droppedCode: 'if [conditional]:'}]};
+  droppedCode: 'if [conditional]:'},
+  {codeTitleText: 'for', 
+  nonCodeTitleText: 'loop', 
+  descriptionText: 'Something about an for loop',
+  droppedCode: 'for [var] in [iterable]:'},
+  {codeTitleText: 'while', 
+  nonCodeTitleText: 'loop', 
+  descriptionText: 'Something about an while loop',
+  droppedCode: 'while [conditional]:'},
+  {codeTitleText: 'print', 
+  nonCodeTitleText: '', 
+  descriptionText: 'Something about printing',
+  droppedCode: 'print([string])'},
+]};
 
 let store: Store<ApplicationState> = createStore(ocSite, initialState);
 
