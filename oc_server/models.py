037fa72bee210b1145c4ae2d52cd65d3b8ab9007
Donny Lawrence <trea@treamous.com>
2017-11-26 20:14:41 -0600

    Add stuff

--- oc_server/models.py
+++ oc_server/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=128)
+    author = models.CharField(max_length=128)
+
+    def __str__(self):
+        return "Course " + self.name + "by" + self.author
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=128)
+    number = models.IntegerField()
+    course = models.ForeignKey('Course', on_delete=models.CASCADE, null=True, related_name="lessons")
+
+    def __str__(self):
+        return "Lesson " + str(self.number) + ": " + self.name
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=128)
+    number = models.IntegerField()
+    text = models.TextField()
+    sections = models.ForeignKey('Lesson', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return "Section " + str(self.number) + ": " + self.name
