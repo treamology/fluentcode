85e6765ef4996d542ce8a3d3b107d7d6c5f886c9
Donny Lawrence <trea@treamous.com>
2017-12-03 21:24:25 -0600

    Make the course details give all of the lesson and section details as well.

--- oc_api/serializers.py
+++ oc_api/serializers.py
@@ -3,21 +3,25 @@ from rest_framework import serializers
 from oc_server import models
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    lessons = serializers.PrimaryKeyRelatedField(many=True, queryset=models.Lesson.objects.all())
-
+class SectionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Course
-        fields = ('id', 'name', 'author', 'lessons')
+        model = models.Section
+        fields = ('id', 'name', 'number', 'text')
+
 
 class LessonSerializer(serializers.ModelSerializer):
-    sections = serializers.PrimaryKeyRelatedField(many=True, queryset=models.Section.objects.all())
+    #sections = serializers.PrimaryKeyRelatedField(many=True, queryset=models.Section.objects.all())
+    sections = SectionSerializer(many=True, read_only=True)
 
     class Meta:
         model = models.Lesson
         fields = ('id', 'name', 'number', 'sections')
 
-class SectionSerializer(serializers.ModelSerializer):
+
+class CourseSerializer(serializers.ModelSerializer):
+    # lessons = serializers.PrimaryKeyRelatedField(many=True, queryset=models.Lesson.objects.all())
+    lessons = LessonSerializer(many=True, read_only=True)
+
     class Meta:
-        model = models.Section
-        fields = ('id', 'name', 'number', 'text')
+        model = models.Course
+        fields = ('id', 'name', 'author', 'lessons')
