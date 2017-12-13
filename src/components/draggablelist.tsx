b51f0792064f71f49e154ad7af54c2a94a39a621
Donny Lawrence <trea@treamous.com>
2017-12-12 19:20:51 -0600

    Make draggables show up per section

--- src/components/draggablelist.tsx
+++ src/components/draggablelist.tsx
@@ -2,20 +2,28 @@ import '../App.scss';
 
 import * as React from 'react';
 
-import { Course } from '../models';
+import { Course, Section, DraggableData } from '../models';
 import Draggable from './draggable';
 import { ApplicationState } from '../state/types/state';
 import { connect } from 'react-redux';
 
 interface DraggableListProps {
     currentCourse: Course
+    currentSection: Section
 }
 
 class UnconnectedDraggableList extends React.Component<DraggableListProps> {
     render() {
+        let draggableData: DraggableData[] = [];
         let draggables: JSX.Element[] = [];
-        if (this.props.currentCourse) {
-            draggables = this.props.currentCourse.draggables.map((codeBlock, index) =>
+
+
+        if (this.props.currentCourse && this.props.currentSection) {
+            for (let id in this.props.currentSection.draggables) {
+                draggableData.push(this.props.currentCourse.draggables[id])
+            }
+
+            draggables = draggableData.map((codeBlock, index) =>
                 <Draggable 
                     codeTitleText={codeBlock.codeName}
                     nonCodeTitleText={codeBlock.descName}
@@ -36,7 +44,8 @@ class UnconnectedDraggableList extends React.Component<DraggableListProps> {
 
 const mapStateToProps = (state: ApplicationState) => {
     return {
-        currentCourse: state.learning.currentCourse
+        currentCourse: state.learning.currentCourse,
+        currentSection: state.learning.currentSection
     };
 };
 
