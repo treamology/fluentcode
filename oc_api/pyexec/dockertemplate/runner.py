a7415da23ddf285e275acbaffd9dac079dbb423a
Donny Lawrence <trea@treamous.com>
2018-01-01 19:13:06 -0600

    Pass the result of the code that was run into the test function.

--- oc_api/pyexec/dockertemplate/runner.py
+++ oc_api/pyexec/dockertemplate/runner.py
@@ -43,7 +43,7 @@ if not mainExecError:
         task = sys.argv[i]
         try:
             exec(task)
-            success = test()
+            success = test(mainExecOutput)
         except Exception:
             print_formatted_exception()
             break
