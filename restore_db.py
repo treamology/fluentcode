7c246cd78b809ab20b36acafc9d5754a8350635d
Donny Lawrence <trea@treamous.com>
2018-01-07 13:24:52 -0600

    Change restore_db.py

--- restore_db.py
+++ restore_db.py
@@ -2,6 +2,6 @@ from subprocess import run
 import sys
 
 run(['export', 'DB_RESTORE=\'' + sys.argv[1] + '\''], shell=True)
-run(['docker-compose', 'up', '-d', 'db'])
+run(['docker-compose', 'up', '-f' 'compose-common.yml', '-d', 'db'])
 run(['docker-compose', 'exec', 'db', 'pg_restore', '-d', 'ocdev', '-U', 'postgres', '-c', '/restore.sql'])
 run(['docker-compose', 'stop', 'db'])
