19d464a6dbb5d87f2059c31535616236e898cab2
Donny Lawrence <trea@treamous.com>
2018-01-10 17:05:52 -0600

    Change some settings.

--- oc_backend/settings.py
+++ oc_backend/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = "nokey4u"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
-ALLOWED_HOSTS = ['fluentcode.xyz']
-
+ALLOWED_HOSTS = ['localhost', 'fluentcode.xyz']
 
 # Application definition
 
