e2b9737bf0fc5de65d00aabca2c70c7fa7958a08
Donny Lawrence <trea@treamous.com>
2018-01-31 18:31:02 -0600

    Add feedback button to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import '../App.scss';
+import '../styles/header.scss';
 
 class Header extends React.Component {
     render() {
         return (
-            <div className="header" />
+            <div className="header">
+                <a href="/feedback" target="_blank" className="feedbackButton">Tester Feedback</a>
+            </div>
         );
     }
 }
