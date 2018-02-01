0b7731ff4377e690a5a120fbebafe1ed11598396
Donny Lawrence <trea@treamous.com>
2018-01-31 18:07:52 -0600

    Add email fields to registration form.

--- oc_server/forms.py
+++ oc_server/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import AuthenticationForm, UsernameField, UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 
 class CustomAuthForm(AuthenticationForm):
@@ -16,6 +17,8 @@ class CustomUserCreationForm(UserCreationForm):
         max_length=254,
         widget=forms.TextInput(attrs={'autofocus': True, 'placeholder': 'Username', 'class': 'textInput'}),
     )
+    email1 = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'E-mail Address', 'class': 'textInput'}))
+    email2 = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Confirm E-mail Address', 'class': 'textInput'}))
     password1 = forms.CharField(
         strip=False,
         widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'textInput'})
@@ -24,3 +27,22 @@ class CustomUserCreationForm(UserCreationForm):
         widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'textInput'}),
         strip=False
     )
+
+    def clean_email2(self):
+        email1 = self.cleaned_data.get('email1')
+        email2 = self.cleaned_data.get('email2')
+        if email1 and email2 and email1 != email2:
+            raise forms.ValidationError(
+                'The two email fields didn\'t match.',
+                code='email_mismatch',
+            )
+        return email2
+
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email2']
+        if commit:
+            user.save()
+        return user
+
+
